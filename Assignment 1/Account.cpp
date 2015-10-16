43d1fa062656676227e7443db1620decd54679ab
Victor Fernandes <victorvalenca@gmail.com>
2015-10-16 17:27:30 -0400

    Input validation
    "IS MAYONNAISE A DOUBLE?"

--- Assignment 1/Account.cpp
+++ Assignment 1/Account.cpp
@@ -15,13 +15,12 @@ void Account::AddItem(){
 	if (numItems){
 		cout << "These are your items for sale:" << endl;
 		Report();
-	} else 
-	{
-		cout << "No items in your account" << endl;
+	} else{
+		cout << "No items in your account." << endl;
 	}
 	char user_select;
 	cout << "Would you like to add another item? (y/N)" << endl;
-	//TODO: Write item entry logic
+
 	fflush(stdin);
 	cin >> user_select;
 	if (toupper(user_select) == 'Y'){
@@ -30,12 +29,37 @@ void Account::AddItem(){
 		cout << "Enter the new Item description: ";
 		fflush(stdin);
 		cin.getline(desc_buffer, 100);
-		cout << "\nEnter the Item Price: ";
-		fflush(stdin);
-		cin >> price_buffer;
 
-		//TODO: Input validation
-	
+		// Set up condition checks
+		bool price_ok = false;
+		bool cancelled = false;
+		
+		// Will continue to run until user gives up or enters a proper value for price
+		while (!price_ok || cancelled){
+			cout << "\nEnter the Item Price (or -1.0 to cancel): ";
+			fflush(stdin);
+			cin >> price_buffer;
+
+			if (!cin.good()){
+				cin.clear();
+				cin.ignore(128,'\n');
+				cout << endl << "Incorrect value. Try again with a proper number";
+				continue;
+			}
+			
+			if (price_buffer == -1.0){
+				cancelled = true;
+				break; // Break out of loop and cancel
+			}
+
+			price_ok = true;
+
+		}
+
+		if (cancelled){
+			return; //Check if user cancelled item entry and exit if so
+		} 
+
 		Item *newItem = new Item();
 		newItem->description = new char[strlen(desc_buffer)+1];
 		newItem->price = price_buffer;
